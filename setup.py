109193022d4d1f6caf973a41f50328b0f4521567
Lukas Muttenthaler <lmuttenthaler@ltgr-hebart6.local>
2021-01-22 15:29:38 +0100

    small fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="thingsvision",
-    version="0.0.5",
+    version="0.0.6",
     author="Lukas Muttenthaler",
     author_email="muttenthaler@cbs.mpg.de",
     description="A library to extract image features from state-of-the-art neural networks for Computer Vision",
