674be1392ce10c0c9d2fe8c6f9007dea5e4d1cb0
Lukas Muttenthaler <lmuttenthaler@ltgr-hebart6.local>
2021-01-29 12:38:40 +0100

    added shell script

---
+++ get_files.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+URLS=(
+	"https://raw.githubusercontent.com/ViCCo-Group/THINGSvision/master/thingsvision/bpe_simple_vocab_16e6.txt.gz"
+	"https://raw.githubusercontent.com/ViCCo-Group/THINGSvision/master/thingsvision/data/item_names.tsv"
+	"https://raw.githubusercontent.com/ViCCo-Group/THINGSvision/master/thingsvision/data/things_concepts.tsv"
+)
+
+FILES=(
+	"bpe_simple_vocab_16e6.txt.gz"
+	"item_names.tsv"
+	"things_concepts.tsv"
+)
+
+v=$(python --version)
+v=$(echo "$v" | cut -c10)
+path="/Users/$(whoami)/anaconda3/lib/python3.$v/site-packages/thingsvision"
+subfolder="/data"
+data_dir="$path$subfolder"
+
+mkdir "$data_dir"
+
+for i in ${!URLS[@]}; do
+	file=${FILES[i]}
+	url=${URLS[i]}
+	curl -O "$url"
+	if [ -f $file ]; then
+		echo "$url successfully downloaded"
+		if [ $i = 0 ]; then
+			mv "$file" "$path"
+		else
+			mv "$file" "$data_dir"
+		fi
+	else
+		echo "$url not successfully downloaded"
+		exit -1
+	fi
+done
+cd ..
