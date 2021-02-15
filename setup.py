dc88b451b50fe569e8c7fa8349f90df94934748c
Lukas Muttenthaler <lmuttenthaler@ltgr-hebart6.local>
2021-02-15 17:28:49 +0100

    updates

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setuptools.setup(
                 name="thingsvision",
-                version="0.4.4",
+                version="0.4.5",
                 author="Lukas Muttenthaler",
                 author_email="muttenthaler@cbs.mpg.de",
                 description="Extracting image features from state-of-the-art neural networks for Computer Vision made easy",
