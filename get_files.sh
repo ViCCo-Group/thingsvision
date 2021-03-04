1935e1269197c3fbc8a386dcce1edd1b5214ec02
Lukas Muttenthaler <lmuttenthaler@ltgr-hebart6.local>
2021-03-04 18:19:46 +0100

    updates

--- get_files.sh
+++ get_files.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 URLS=(
-	"https://raw.githubusercontent.com/ViCCo-Group/THINGSvision/master/thingsvision/bpe_simple_vocab_16e6.txt.gz"
+	#"https://raw.githubusercontent.com/ViCCo-Group/THINGSvision/master/thingsvision/bpe_simple_vocab_16e6.txt.gz"
 	"https://raw.githubusercontent.com/ViCCo-Group/THINGSvision/master/thingsvision/data/item_names.tsv"
 	"https://raw.githubusercontent.com/ViCCo-Group/THINGSvision/master/thingsvision/data/things_concepts.tsv"
 )
 
 FILES=(
-	"bpe_simple_vocab_16e6.txt.gz"
+	#"bpe_simple_vocab_16e6.txt.gz"
 	"item_names.tsv"
 	"things_concepts.tsv"
 )
 
 v=$(python --version)
 v=$(echo "$v" | cut -c10)
-path="/Users/$(whoami)/anaconda3/lib/python3.$v/site-packages/thingsvision/"
+#path="/Users/$(whoami)/anaconda3/lib/python3.$v/site-packages/thingsvision/"
 subfolder="$(pwd)/data/"
 
 if [[ -d $subfolder ]]; then
@@ -29,11 +29,11 @@ for i in ${!URLS[@]}; do
 	curl -O "$url"
 	if [[ -f $file ]]; then
 		echo "$url successfully downloaded"
-		if [[ $i -eq 0 ]]; then
-			mv "$file" "$path"
-		else
-			mv "$file" "$subfolder"
-		fi
+		#if [[ $i -eq 0 ]]; then
+		#	mv "$file" "$path"
+		#else
+		mv "$file" "$subfolder"
+		#fi
 	else
 		echo "$url not successfully downloaded"
 		exit -1
