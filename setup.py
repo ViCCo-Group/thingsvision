0aa35b6967bc92d39b30a0967f36524520cf1961
LukasMut <muttenthaler@cbs.mpg.de>
2021-07-14 10:12:29 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ requirements = [
                 'tqdm',
                 'regex',
                 'scikit-image',
+                'scikit-learn',
                 'scipy',
                 'h5py',
                 ]
