946f6283942309589297b9a63e50608a994f9427
LukasMut <muttenthaler@cbs.mpg.de>
2021-07-28 16:17:18 +0200

    refactor

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setuptools.setup(
                 name="thingsvision",
-                version="1.0.1",
+                version="1.0.2",
                 author="Lukas Muttenthaler",
                 author_email="muttenthaler@cbs.mpg.de",
                 description="Extracting image features from state-of-the-art neural networks for Computer Vision made easy",
