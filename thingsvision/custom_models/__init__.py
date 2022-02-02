75416a31b786d67224c37c16a661f36916f79054
Hannes Hansen <mail@hannesh.de>
2022-02-02 21:39:03 +0100

    updated alexnet ecoset model weight path

--- thingsvision/custom_models/__init__.py
+++ thingsvision/custom_models/__init__.py
@@ -1,2 +1,3 @@
 from thingsvision.custom_models.resnet50_ecoset import Resnet50_ecoset
 from thingsvision.custom_models.vgg16bn_ecoset import VGG16bn_ecoset
+from thingsvision.custom_models.alexnet_ecoset import Alexnet_ecoset
