495e4faa15cec694e2b6db376b8742bec409ad1b
LukasMut <muttenthaler@cbs.mpg.de>
2022-09-26 14:22:22 +0200

    updated patch version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requirements = [
 
 setuptools.setup(
                 name="thingsvision",
-                version="2.1.3",
+                version="2.1.4",
                 author="Lukas Muttenthaler",
                 author_email="muttenthaler@cbs.mpg.de",
                 description="Extracting image features from state-of-the-art neural networks for Computer Vision made easy",
