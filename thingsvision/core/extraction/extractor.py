85a5892efa9aec9651b14840969b95328d1a6ac4
Jonas Dippel <jonas.dippel@gmx.de>
2022-11-01 10:15:24 +0100

    added vissl extractor

--- thingsvision/core/extraction/extractor.py
+++ thingsvision/core/extraction/extractor.py
@@ -7,6 +7,12 @@ import tensorflow.keras.applications as tensorflow_models
 import timm
 import torch
 import torchvision
+import os
+
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 from .base import BaseExtractor
 from .mixin import PyTorchMixin, TensorFlowMixin
@@ -166,4 +172,97 @@ class KerasExtractor(BaseExtractor, TensorFlowMixin):
         else:
             raise ValueError(
                 f"\nCould not find {self.model_name} among TensorFlow models.\n"
+            )
+
+
+@dataclass(repr=True)
+class VisslExtractor(BaseExtractor, PyTorchMixin):
+    ENV_TORCH_HOME = 'TORCH_HOME'
+    ENV_XDG_CACHE_HOME = 'XDG_CACHE_HOME'
+    DEFAULT_CACHE_DIR = '~/.cache'
+    MODELS = {
+        'simclr-rn50-e800': {
+            'url': 'https://dl.fbaipublicfiles.com/vissl/model_zoo/simclr_rn50_800ep_simclr_8node_resnet_16_07_20.7e8feed1/model_final_checkpoint_phase799.torch',
+            'arch': 'resnet50'
+        },
+        'mocov2-rn50-e200': {
+            'url': 'https://dl.fbaipublicfiles.com/vissl/model_zoo/moco_v2_1node_lr.03_step_b32_zero_init/model_final_checkpoint_phase199.torch',
+            'arch': 'resnet50'
+        }
+    }
+
+    def __init__(
+            self,
+            model_name: str,
+            pretrained: bool,
+            device: str,
+            model_path: str = None,
+            model_parameters: Dict = None,
+            preprocess: Any = None,
+    ) -> None:
+        super().__init__(
+            model_name=model_name,
+            pretrained=pretrained,
+            model_path=model_path,
+            model_parameters=model_parameters,
+            preprocess=preprocess,
+            device=device,
+        )
+
+    def _download_and_save_model(self, model_url: str, output_model_filepath: str):
+        """Downloads"""
+        model = load_state_dict_from_url(model_url, map_location=torch.device('cpu'))
+
+        # get the model trunk to rename
+        if "classy_state_dict" in model.keys():
+            model_trunk = model["classy_state_dict"]["base_model"]["model"]["trunk"]
+        elif "model_state_dict" in model.keys():
+            model_trunk = model["model_state_dict"]
+        else:
+            model_trunk = model
+
+        converted_model = self._replace_module_prefix(model_trunk, "_feature_blocks.")
+        torch.save(converted_model, output_model_filepath)
+        return converted_model
+
+    def _replace_module_prefix(self, state_dict: Dict[str, Any],
+                               prefix: str,
+                               replace_with: str = ""):
+        """
+        Remove prefixes in a state_dict needed when loading models that are not VISSL
+        trained models.
+        Specify the prefix in the keys that should be removed.
+        """
+        state_dict = {
+            (key.replace(prefix, replace_with, 1) if key.startswith(prefix) else key): val
+            for (key, val) in state_dict.items()
+        }
+        return state_dict
+
+    def _get_torch_home(self):
+        """Gets the torch home folder used as a cache directory for the vissl models"""
+        torch_home = os.path.expanduser(
+            os.getenv(VisslExtractor.ENV_TORCH_HOME,
+                      os.path.join(os.getenv(VisslExtractor.ENV_XDG_CACHE_HOME,
+                                             VisslExtractor.DEFAULT_CACHE_DIR), 'torch')))
+        return torch_home
+
+    def load_model_from_source(self) -> None:
+        """Load a (pretrained) neural network model from vissl."""
+        if self.model_name in VisslExtractor.MODELS:
+            cache_dir = os.path.join(self._get_torch_home(), 'vissl')
+            model_filepath = os.path.join(cache_dir, self.model_name + '.torch')
+            model_config = VisslExtractor.MODELS[self.model_name]
+            if not os.path.exists(model_filepath):
+                os.makedirs(cache_dir, exist_ok=True)
+                model_state_dict = self._download_and_save_model(model_url=model_config['url'],
+                                                                 output_model_filepath=model_filepath)
+            else:
+                model_state_dict = torch.load(model_filepath, map_location=torch.device('cpu'))
+            self.model = getattr(torchvision.models, model_config['arch'])()
+            self.model.fc = torch.nn.Identity()
+            self.model.load_state_dict(model_state_dict, strict=True)
+        else:
+            raise ValueError(
+                f"\nCould not find {self.model_name} among in the Vissl library.\n"
             )
