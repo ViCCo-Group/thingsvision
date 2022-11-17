63eb85db6882f6419a3dc4c2252dc70c3fd55538
Lukas Muttenthaler <LukasMut@users.noreply.github.com>
2022-11-17 17:54:15 +0100

    Update __init__.py

--- thingsvision/custom_models/__init__.py
+++ thingsvision/custom_models/__init__.py
@@ -2,6 +2,7 @@ from .alexnet_ecoset import Alexnet_ecoset
 from .barlowtwins import BarlowTwins
 from .resnet50_ecoset import Resnet50_ecoset
 from .swav import Swav
+from .vicreg import Vicreg
 from .vgg16_ecoset import VGG16_ecoset
 from .openclip import OpenCLIP
 from .inception_ecoset import Inception_ecoset
