0a4979f1be7b4fd1c23f05abe84a29207829ded1
Alex Murphy <murphyalex@gmail.com>
2023-02-19 13:12:37 -0700

    Add back in accidentally-removed line

---
+++ build/lib/tests/extractor/test_transformations.py
@@ -0,0 +1,26 @@
+import unittest
+
+import tensorflow as tf
+import tests.helper as helper
+from torchvision import transforms as T
+
+
+class ModelTransformationsTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        helper.create_test_images()
+
+    def test_transformations_cnn(self):
+        model_name = "vgg16_bn"
+        extractor, _, _ = helper.create_extractor_and_dataloader(
+            model_name, pretrained=False, source="torchvision"
+        )
+        transforms = extractor.get_transformations()
+        self.assertTrue(isinstance(transforms, T.Compose))
+
+        model_name = "VGG16"
+        extractor, _, _ = helper.create_extractor_and_dataloader(
+            model_name, pretrained=False, source="keras"
+        )
+        transforms = extractor.get_transformations()
+        self.assertTrue(isinstance(transforms, tf.keras.Sequential))
