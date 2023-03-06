6c6ae5395066ab11a392d48490e7ccf150d1f502
Lukas Muttenthaler <LukasMut@users.noreply.github.com>
2023-03-06 12:02:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [
     "tensorflow==2.9.* ; sys_platform != 'darwin' or platform_machine != 'arm64'",
     "tensorflow-macos==2.9.* ; sys_platform == 'darwin' and platform_machine == 'arm64'",
     "timm==0.6.*",
-    "torch>=1.13.1",
+    "torch>=1.13.*",
     "torchvision==0.14.*",
     "torchtyping",
     "tqdm",
