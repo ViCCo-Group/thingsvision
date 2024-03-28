44a9d2dc3ef14b95fa004fc1a5b9fa30e5dc9927
LukasMut <muttenthaler@cbs.mpg.de>
2024-03-28 14:06:51 +0100

    made transform more abstract

--- thingsvision/core/extraction/torch.py
+++ thingsvision/core/extraction/torch.py
@@ -163,12 +163,12 @@ class PyTorchExtractor(BaseExtractor):
     ) -> Union[Tensor, Array]:
         if self.model_name == "OpenCLIP":
             base_model = self.model_name
-            variant = self.model_parameters["variant"]
-            dataset = self.model_parameters["dataset"]
+            variant = self.model_parameters.get("variant") 
+            dataset = self.model_parameters.get("dataset")
             model_name = "_".join((base_model, variant, dataset))
         elif self.model_name == "clip" or self.model_name == "DreamSim":
             base_model = self.model_name
-            variant = self.model_parameters["variant"]
+            variant = self.model_parameters.get("variant")
             model_name = "_".join((base_model, variant))
         else:
             model_name = self.model_name
