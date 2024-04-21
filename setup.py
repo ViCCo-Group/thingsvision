337ca61c6900da7cfbc4cfbee4f21d311a442a56
LukasMut <muttenthaler@cbs.mpg.de>
2024-04-21 17:45:43 +0200

    setup and requirements match

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
     "tensorflow==2.9.* ; sys_platform != 'darwin' or platform_machine != 'arm64'",
     "tensorflow-macos==2.9.* ; sys_platform == 'darwin' and platform_machine == 'arm64'",
     "timm",
-    "torch>=2.0.1",
+    "torch>=2.0.0",
     "torchvision==0.15.2",
     "torchtyping",
     "tqdm",
